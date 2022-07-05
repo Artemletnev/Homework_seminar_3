0b19a362c26705f9b15f70eef1e465b98b90fb8c
Artem <letnev2012@gmail.com>
2022-07-05 15:41:42 +0300

    Add task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,7 @@
+﻿ Console.WriteLine("Enter the number ");
+ int N = Convert.ToInt32(Console.ReadLine());
+
+ for (int i = 1; i <= N; i++)
+ {
+    Console.WriteLine($"cube of number {i} = {i*i*i}");
+ }
